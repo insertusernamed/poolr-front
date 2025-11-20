acb261ba93a44ffb2bcd303fae4731a6c2b41954
insertusernamed <yevtushenkodan04@gmail.com>
2025-11-20 18:43:49 -0500

    Added demo account button and restyled login and register views

--- src/components/DemoAccountButton.vue
+++ src/components/DemoAccountButton.vue
@@ -2,10 +2,10 @@
     <div class="mt-6 text-center">
         <div class="relative">
             <div class="absolute inset-0 flex items-center">
-                <div class="w-full border-t border-gray-600"></div>
+                <div class="w-full border-t border-slate-200"></div>
             </div>
             <div class="relative flex justify-center text-sm">
-                <span class="px-2 bg-sky-900 text-gray-400">Or try it out</span>
+                <span class="px-2 bg-white text-slate-500">Or try it out</span>
             </div>
         </div>
 
@@ -14,7 +14,7 @@
                 class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 Create Demo Account
             </button>
-            <p class="mt-2 text-xs text-gray-400">
+            <p class="mt-2 text-xs text-slate-400">
                 Demo accounts last for 1 hour. All changes will be reverted afterwards.
             </p>
         </div>
